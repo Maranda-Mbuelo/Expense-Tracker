7704232833083567490eaef8cfb0d8165299aab3
Mbuelo <brightmarsh.m>
2023-08-27 00:16:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//Dipendency Injection
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder
+    .Configuration.GetConnectionString("DevConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
